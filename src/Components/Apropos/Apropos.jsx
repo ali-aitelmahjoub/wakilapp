fdff031524c8d178d980b2c27dda41283271f79a
ali-aitelmahjoub <aitelmahjoubali6@gmail.com>
2022-01-20 21:43:13 +0000

    create apropos page

---
+++ src/Components/Apropos/Apropos.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './apropos.css'
+
+const Apropos = () => {
+  return <div className='aproposContainer'>
+      <div className="aproposWrap">
+       <div className="box info">
+           <h2 className='miniTitle item1'>Oue Service</h2>
+           <h1 className='infoTitle'>What We Offre</h1>
+           <p className='infoPara'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+       </div>
+       <div className="box aproposItem">
+           <h1 className='infoLogo item'><i class="fas fa-book-open"></i></h1>
+           <h2 className='boxTitle item'>OPTIMISEZ VOTRE AGENDA</h2>
+           <p className='boxPara item'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+       </div>
+       <div className="box aproposItem">
+       <h1 className='infoLogo item'><i class="fas fa-atom"></i></h1>
+           <h2 className='boxTitle item'>PAS D'ENGAGEMENT</h2>
+           <p className='boxPara item'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+       </div>
+       <div className="box aproposItem">
+       <h1 className='infoLogo item'><i class="fas fa-gift"></i></h1>
+           <h2 className='boxTitle item'>RELATION PRIVILÉGIÉE</h2>
+           <p className='boxPara item'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+       </div>
+       <div className="box aproposItem">
+           <h1 className='infoLogo item'><i class="fas fa-book-open"></i></h1>
+           <h2 className='boxTitle item'>OPTIMISEZ VOTRE AGENDA</h2>
+           <p className='boxPara item'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+       </div>
+       <div className="box aproposItem">
+           <h1 className='infoLogo item'><i class="fas fa-book-open"></i></h1>
+           <h2 className='boxTitle item'>OPTIMISEZ VOTRE AGENDA</h2>
+           <p className='boxPara item'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+       </div>
+       </div>
+  </div>;
+};
+
+export default Apropos;
